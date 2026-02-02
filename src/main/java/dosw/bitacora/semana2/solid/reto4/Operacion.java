98397bf38b434ccfd8e5ced40e866233b709fdd1
Nicolas Parrado <nicoparrado@example.com>
2026-02-02 17:59:19 -0500

    cambio semana dos

---
+++ src/main/java/dosw/bitacora/semana2/solid/reto4/Operacion.java
@@ -0,0 +1,5 @@
+package dosw.bitacora.semana2.solid.reto4;
+
+public interface Operacion {
+    double ejecutar(double a, double b);
+}
---
+++ src/main/java/dosw/bitacora/semana2/solid/reto4/Suma.java
@@ -0,0 +1,8 @@
+package dosw.bitacora.semana2.solid.reto4;
+
+public class Suma implements Operacion {
+    @Override
+    public double ejecutar(double a, double b) {
+        return a + b;
+    }
+}
