108a8c9b901058fdc95b0bfe121b42b67ed3bf2a
Nicolas Parrado <nicolas.parrado-g@mail.escuelaing.edu.co>
2026-03-14 16:52:48 -0500

    limpieza rama semana 8

--- src/main/java/ejercicio1/Notificonico.java
+++
@@ -1,6 +0,0 @@
-package ejercicio1;
-
-// interfaz para las diferentes formas de enviar notificaciones
-public interface Notificonico {
-    void enviar(String mensaje, String destinatario);
-}
--- src/main/java/ejercicio2/Procesadornico.java
+++
@@ -1,6 +0,0 @@
-package ejercicio2;
-
-// interfaz para los diferentes procesadores de pago
-public interface Procesadornico {
-    boolean procesarPago(double monto, String cuenta);
-}
